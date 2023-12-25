8644cd124f62834ff06be58a5171226e0987a444
abu huraira <103133051+Xstran@users.noreply.github.com>
2023-12-25 13:52:21 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone! my name is huraira an this is ec2 test'
+    return 'Hello, everyone! my name is huraira and this is testing on ec2'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
