fac7ea386c539df555890633491a2fffe49e1f6d
abu huraira <103133051+Xstran@users.noreply.github.com>
2023-12-26 18:07:14 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone! my name is huraira and this is testing'
+    return render_template('index.html', name='Huraira')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    user_name = request.form['name']
+    return render_template('index.html', name=user_name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
